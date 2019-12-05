7cbf86e33ea9ae0daac88de1dcf69b5183888a09
git-ankita <ankitachatt88@gmail.com>
2019-12-05 21:17:42 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+ //template: `<app-server></app-server>
+ //<app-servers></app-servers>`,
+  //styleUrls: ['./app.component.css']
+  styles:[`
+  h1{
+    color: brown;
+}
+h3{
+    color: burlywood;
+}
+  
+  `]
+})
+export class AppComponent {
+  
+}
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+
+import { Component } from '@angular/core';
+
+@Component({ 
+    selector : 'app-server',
+    templateUrl: './server.component.html',
+    styles:[`
+    h1{
+        color: blue;
+    }`]
+
+})
+
+export class ServerComponent {
+
+}
+
